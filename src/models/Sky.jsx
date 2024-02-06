de578c8d065fcfdbb445223787d87b380c06913b
Nguyen2705 <minhvu0312@gmail.com>
2024-02-05 19:07:01 -0600

    Update New Background For The Scene

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -15,7 +15,7 @@ const Sky = ({ isRotating }) => {
   })
 
   return (
-    <mesh ref={skyRef}>
+    <mesh ref={skyRef} scale={[100, 100, 100]} position={[0, 0 ,-351]}>
         <primitive object={sky.scene} />
     </mesh>
   )
